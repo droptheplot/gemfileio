70a6e6b2c9690e4672d68a7ee480387d5f3b56f3
Sergey Novikov <novikov359@gmail.com>
2015-12-16 22:19:56 +0300

    Init angularjs

---
+++ app/assets/javascripts/controllers/application_controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gemfileio')
+    .controller('applicationController', applicationController);
+
+  applicationController.$inject = ['$scope'];
+
+  function applicationController($scope) {
+  }
+})();
