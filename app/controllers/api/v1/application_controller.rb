def2dc752bacc6003a67b6b19465353ba0aef3ec
Sergey Novikov <novikov359@gmail.com>
2015-12-16 22:07:13 +0300

    Add api controller and routes

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::ApplicationController < ActionController::Base
+  include ActionController::Serialization
+
+  before_filter :set_headers, :set_current_user
+
+  serialization_scope :view_context
+
+  def not_found
+    render json: { 'error' => 'Not found' }, :status => 404
+  end
+
+  def cors_preflight
+    head :ok if request.request_method == 'OPTIONS'
+  end
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization'
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Request-Method'] = '*'
+  end
+
+  def authenticate!
+    head :unauthorized unless current_user
+  end
+
+  def has_permissions user
+    head :unauthorized unless user == current_user
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def set_current_user
+    token = request.headers['Authorization'].to_s.split(' ').last
+
+    return unless token
+
+    payload = JWT.decode(token, Rails.application.secrets.jwt_secret, 'HS512').first rescue nil
+
+    return unless payload
+
+    if payload && Time.now < Time.at(payload['exp'].to_i)
+      @current_user = User.find(payload['user']['id'])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      match '*path' => 'application#not_found', :via => [:get, :post]
+      match '*path' => 'application#cors_preflight', :via => :options
+    end
+  end
+
+  get '(*page)' => 'pages#index'
 end
