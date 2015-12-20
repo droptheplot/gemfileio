313b9be4e7cdee382df7e18539d45f68b691e6fd
Sergey Novikov <novikov359@gmail.com>
2015-12-20 23:47:44 +0300

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+lock '3.4.0'
+
+set :application, 'gemfileio'
+set :repo_url, 'git@github.com:droptheplot/gemfileio.git'
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/gemfileio'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'vendor/assets/components')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  before 'deploy:assets:precompile', :bower_install do
+    on roles :web do
+      within release_path do
+        execute :bower, 'install'
+      end
+    end
+  end
+end
