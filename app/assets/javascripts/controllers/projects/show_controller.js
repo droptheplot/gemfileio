b7004a7fc26d054eae6229ab1dd85f15de424406
Sergey Novikov <novikov359@gmail.com>
2015-12-20 17:40:08 +0300

    Update project show template

--- app/assets/javascripts/controllers/projects/show_controller.js
+++ app/assets/javascripts/controllers/projects/show_controller.js
@@ -8,8 +8,6 @@
   projectsShowController.$inject = ['$scope', 'projectsFactory'];
 
   function projectsShowController($scope, projectsFactory) {
-    $scope.showdown = new showdown.Converter();
-
     projectsFactory.get({ id: $scope.ngDialogData.projectId }, function(data) {
       $scope.project = data;
     });
