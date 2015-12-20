996fde38d42d4ea8097f9ff9c18f2f29ad481a36
Sergey Novikov <novikov359@gmail.com>
2015-12-20 12:16:33 +0300

    Fix counter_cache for projects categories if active

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,15 @@
 class Project < ActiveRecord::Base
-  belongs_to :category, counter_cache: true
+  belongs_to :category
 
   validates_presence_of :name
   validates_uniqueness_of :name
 
   scope :active, ->{ where(active: true) }
   scope :top, ->{ order(stars_count: :desc, forks_count: :desc, downloads_count: :desc) }
+
+  after_save :increment_category_counter_cache, if: -> (project) { project.active? && project.active_changed? }
+  after_update :decrement_category_counter_cache, unless: :active?, if: :active_changed?
+  after_destroy :decrement_category_counter_cache, if: :active?
 
   def ref
     [self.owner, self.repo].join('/') if self.owner && self.repo
@@ -14,4 +18,14 @@ class Project < ActiveRecord::Base
   def url
     URI.join('https://github.com', self.ref).to_s if self.ref
   end
+
+  private
+
+    def increment_category_counter_cache
+      Category.increment_counter('projects_count', self.category.id)
+    end
+
+    def decrement_category_counter_cache
+      Category.decrement_counter('projects_count', self.category.id)
+    end
 end
