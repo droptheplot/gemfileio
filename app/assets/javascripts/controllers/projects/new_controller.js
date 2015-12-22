124a7f5209058cace908b731dd09d2ca9ac708dc
Sergey Novikov <novikov359@gmail.com>
2015-12-22 21:27:48 +0300

    Make project active of it is submitted by admin

--- app/assets/javascripts/controllers/projects/new_controller.js
+++ app/assets/javascripts/controllers/projects/new_controller.js
@@ -5,14 +5,22 @@
     .module('gemfileio')
     .controller('projectsNewController', projectsNewController);
 
-  projectsNewController.$inject = ['$scope', 'projectsFactory', 'categoriesFactory', '$location', 'notificationsFactory'];
+  projectsNewController.$inject = ['$scope', '$rootScope', 'projectsFactory', 'categoriesFactory', '$location', 'notificationsFactory'];
 
-  function projectsNewController($scope, projectsFactory, categoriesFactory, $location, notificationsFactory) {
+  function projectsNewController($scope, $rootScope, projectsFactory, categoriesFactory, $location, notificationsFactory) {
+    $scope.project = {};
+
     categoriesFactory.query({}, function(data) {
       $scope.categories = data;
+      $scope.project.category_id = $scope.categories[0].id;
     });
 
     $scope.submitProject = function(project) {
+      if($rootScope.currentUser.admin) {
+        project.active = true;
+      }
+      console.log(project)
+
       projectsFactory.save(project, function(data) {
         $location.path('/');
         notificationsFactory.add('Thanks!');
