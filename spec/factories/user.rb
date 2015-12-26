23cbf9f48dd1b10f30133a628b011bd80ce9e965
Sergey Novikov <novikov359@gmail.com>
2015-12-26 16:01:35 +0300

    Add create and update project specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     name { Faker::Lorem.word }
     provider 'github'
     uid { Faker::Number.number(32) }
+
+    trait :admin do
+      admin true
+    end
   end
 end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe 'Projects', :type => :request do
-  let!(:user) { FactoryGirl.create(:user) }
-  let(:headers) {{ 'Authorization' => user.token }}
-
   describe "GET 'index'" do
     let!(:projects) { FactoryGirl.create_list(:project, 5) }
 
@@ -23,6 +20,35 @@ describe 'Projects', :type => :request do
 
       expect(response).to be_success
       expect(json).to_not be_nil
+    end
+  end
+
+  describe "POST 'create'" do
+    let!(:category) { FactoryGirl.create(:category) }
+    let(:params) {{ name: 'rails', category_id: category.id }}
+
+    it 'should create project' do
+      expect {
+        post api_v1_projects_path, params, headers
+      }.to change(Project, :count).by(1)
+
+      expect(response).to be_success
+    end
+  end
+
+  describe "POST 'update'" do
+    let!(:project) { FactoryGirl.create(:project) }
+    let!(:user) { FactoryGirl.create(:user, :admin) }
+    let(:params) {{ name: 'rake' }}
+    let(:headers) {{ 'Authorization' => user.token }}
+
+    it 'should update project' do
+      put api_v1_project_path(project), params, headers
+
+      project.reload
+
+      expect(project.name).to eq('rake')
+      expect(response).to be_success
     end
   end
 end
