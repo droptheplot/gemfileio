10f582c88f870bbc7fbb061979570df71136db1c
Sergey Novikov <novikov359@gmail.com>
2015-12-30 00:56:10 +0300

    Add create, update, toggle_favorite methods for projects to api docs

--- config/initializers/apipony.rb
+++ config/initializers/apipony.rb
@@ -6,11 +6,14 @@ Apipony::Documentation.define do
 
   section 'Projects' do
     endpoint 'get', '/projects' do |e|
+      e.description = 'Get projects'
+
       request_with do
         param :active, example: true
         param :category_id, example: 1
         param :query, example: 'rails'
         param :favorited_by, example: 12
+        param :page, example: 1
       end
 
       response_with 200, array: true do
@@ -31,11 +34,100 @@ Apipony::Documentation.define do
     end
 
     endpoint 'get', '/projects/:id' do |e|
+      e.description = 'Get project by id'
+
       request_with do
         param :id, example: 1, required: true
       end
 
-      response_with 200, array: true do
+      response_with 200 do
+        attribute :id, type: :integer, example: 1
+        attribute :name, type: :string, example: 'rails'
+        attribute :owner, type: :string, example: 'rails'
+        attribute :repo, type: :string, example: 'rails'
+        attribute :stars_count, type: :integer, example: 28913
+        attribute :forks_count, type: :integer, example: 11710
+        attribute :downloads_count, type: :integer, example: 58617256
+        attribute :comments_count, type: :integer, example: 5
+        attribute :description, type: :string, example: 'Ruby on Rails'
+        attribute :ref, type: :string, example: 'rails/rails'
+        attribute :url, type: :string, example: 'https//github.com/rails/rails'
+        attribute :readme, type: :string, example: '## Welcome to Rails...'
+        attribute :active, type: :boolean, example: true
+        attribute :category_id, type: :integer, example: 1
+        attribute :favorited_by_current_user, type: :boolean, example: false
+      end
+    end
+
+    endpoint 'post', '/projects' do |e|
+      e.description = 'Create project'
+
+      request_with do
+        param :name, example: 'rails', required: true
+        param :owner, example: 'rails', required: true
+        param :repo, example: 'rails', required: true
+        param :active, example: true
+        param :category_id, example: 1
+      end
+
+      response_with 200 do
+        attribute :id, type: :integer, example: 1
+        attribute :name, type: :string, example: 'rails'
+        attribute :owner, type: :string, example: 'rails'
+        attribute :repo, type: :string, example: 'rails'
+        attribute :stars_count, type: :integer, example: 28913
+        attribute :forks_count, type: :integer, example: 11710
+        attribute :downloads_count, type: :integer, example: 58617256
+        attribute :comments_count, type: :integer, example: 5
+        attribute :description, type: :string, example: 'Ruby on Rails'
+        attribute :ref, type: :string, example: 'rails/rails'
+        attribute :url, type: :string, example: 'https//github.com/rails/rails'
+        attribute :readme, type: :string, example: '## Welcome to Rails...'
+        attribute :active, type: :boolean, example: true
+        attribute :category_id, type: :integer, example: 1
+        attribute :favorited_by_current_user, type: :boolean, example: false
+      end
+    end
+
+    endpoint 'put', '/projects/:id' do |e|
+      e.description = 'Update project'
+
+      request_with do
+        param :id, example: 1, required: true
+        param :name, example: 'rails', required: true
+        param :owner, example: 'rails', required: true
+        param :repo, example: 'rails', required: true
+        param :active, example: true
+        param :category_id, example: 1
+      end
+
+      response_with 200 do
+        attribute :id, type: :integer, example: 1
+        attribute :name, type: :string, example: 'rails'
+        attribute :owner, type: :string, example: 'rails'
+        attribute :repo, type: :string, example: 'rails'
+        attribute :stars_count, type: :integer, example: 28913
+        attribute :forks_count, type: :integer, example: 11710
+        attribute :downloads_count, type: :integer, example: 58617256
+        attribute :comments_count, type: :integer, example: 5
+        attribute :description, type: :string, example: 'Ruby on Rails'
+        attribute :ref, type: :string, example: 'rails/rails'
+        attribute :url, type: :string, example: 'https//github.com/rails/rails'
+        attribute :readme, type: :string, example: '## Welcome to Rails...'
+        attribute :active, type: :boolean, example: true
+        attribute :category_id, type: :integer, example: 1
+        attribute :favorited_by_current_user, type: :boolean, example: false
+      end
+    end
+
+    endpoint 'post', '/projects/:id/toggle_favorite' do |e|
+      e.description = 'Toggle project in favorites for current user'
+
+      request_with do
+        param :id, example: 1, required: true
+      end
+
+      response_with 200 do
         attribute :id, type: :integer, example: 1
         attribute :name, type: :string, example: 'rails'
         attribute :owner, type: :string, example: 'rails'
