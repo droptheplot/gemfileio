3bb0bddb956051ebbec6d116f4435299f1f83800
Sergey Novikov <novikov359@gmail.com>
2016-01-03 16:52:30 +0300

    Add spec if comment is not created

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -19,11 +19,20 @@ describe 'Comments', type: :request do
     let(:params) {{ body: 'comment', project_id: project.id }}
 
     it 'should create comment for project' do
+
       expect {
         post api_v1_comments_path, params, headers
       }.to change(Comment, :count).by(1)
 
       expect(response).to be_success
+    end
+
+    it 'should respond with error if comment is not created' do
+      expect {
+        post api_v1_comments_path, {}, headers
+      }.not_to change(Comment, :count)
+
+      expect(response).to be_unprocessable
     end
   end
 end
