a11f05fe9592aae14331e699befca4474d47a78f
Sergey Novikov <novikov359@gmail.com>
2016-01-06 17:31:15 +0300

    Change order and limit for statistics api

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::StatisticsController < Api::V1::ApplicationController
   def index
-    @statistics = Statistic.order(id: :desc)
+    @statistics = Statistic.order(created_at: :asc).limit(30)
 
     render json: @statistics, each_serializer: StatisticSerializer
   end
