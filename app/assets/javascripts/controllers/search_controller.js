7af9615096cd4c9348a6e4b753e9ffff8ddc4498
Sergey Novikov <novikov359@gmail.com>
2016-01-10 13:57:07 +0300

    Add query to scope when searchController is initialized

--- app/assets/javascripts/controllers/search_controller.js
+++ app/assets/javascripts/controllers/search_controller.js
@@ -8,6 +8,8 @@
   searchController.$inject = ['$scope', '$location', '$routeParams'];
 
   function searchController($scope, $location, $routeParams) {
+    $scope.query = $location.search().query;
+    
     $scope.$on('$locationChangeSuccess', function() {
       if($location.search().query !== '') {
         $scope.query = $location.search().query;
