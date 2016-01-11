0c2fcfef27061f2f2511ef27d2be615c18df9894
Sergey Novikov <novikov359@gmail.com>
2016-01-11 20:47:03 +0300

    Add subcategories to aside

--- app/assets/javascripts/controllers/categories_controller.js
+++ app/assets/javascripts/controllers/categories_controller.js
@@ -5,12 +5,25 @@
     .module('gemfileio')
     .controller('categoriesController', categoriesController);
 
-  categoriesController.$inject = ['$scope', 'categoriesFactory'];
+  categoriesController.$inject = ['$scope', '$location', 'categoriesFactory'];
 
-  function categoriesController($scope, categoriesFactory) {
+  function categoriesController($scope, $location, categoriesFactory) {
     categoriesFactory.query({}, function(data) {
       $scope.categories = data;
     });
+
+    $scope.showCategory = function(category) {
+      if(category.id === $scope.showCategoryId) {
+        delete $scope.showCategoryId;
+        return;
+      }
+
+      if(category.subcategories.length > 0) {
+        $scope.showCategoryId = category.id;
+      } else {
+        $location.path('/categories/' + category.id);
+      }
+    };
 
     $scope.$on('$routeChangeSuccess', function(event, current, previous) {
       if(current.params.category_id) {
