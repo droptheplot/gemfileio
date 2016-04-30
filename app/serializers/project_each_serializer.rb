19893f8870eab96757b0ca90143d7a1157c19b42
Sergey Novikov <novikov359@gmail.com>
2016-04-30 21:58:55 +0300

    Cleanup project serializers

--- app/serializers/project_each_serializer.rb
+++ app/serializers/project_each_serializer.rb
@@ -1,22 +1,21 @@
 class ProjectEachSerializer < ActiveModel::Serializer
-  attributes *%i[
-    id
-    name
-    owner
-    repo
-    stars_count
-    forks_count
-    contributors_count
-    downloads_count
-    comments_count
-    description
-    github_name
-    github_url
-    url
-    active
-    category_id
-    activity
-  ]
+  attributes(
+    *%i[
+      id
+      name
+      owner
+      repo
+      stars_count
+      forks_count
+      contributors_count
+      downloads_count
+      comments_count
+      description
+      github_name
+      github_url
+      url
+    ]
+  )
 
   def url
     project_path(self.id)
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,27 +1,20 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes *%i[
-    id
-    name
-    owner
-    repo
-    stars_count
-    forks_count
-    contributors_count
-    views_count
-    downloads_count
-    comments_count
-    description
-    github_name
-    url
-    active
-    category_id
-    commits_count
-    activity
-  ]
-
-  has_many :comments
-
-  def comments
-   object.comments.joins(:user)
-  end
+  attributes(
+    *%i[
+      id
+      name
+      owner
+      repo
+      stars_count
+      forks_count
+      contributors_count
+      views_count
+      downloads_count
+      comments_count
+      description
+      github_name
+      url
+      commits_count
+    ]
+  )
 end
