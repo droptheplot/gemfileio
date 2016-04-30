af5bb7c9c278b90bd0c5397cffbfc72f6af23cca
Sergey Novikov <novikov359@gmail.com>
2016-04-30 15:30:06 +0300

    Remove user from api application controller

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::ApplicationController < ActionController::Base
   include ActionController::Serialization
 
-  before_filter :set_headers, :set_current_user
-
-  serialization_scope :current_user
+  before_filter :set_headers
 
   def not_found
-    render json: { 'error' => 'Not found' }, :status => 404
+    render json: { error: 'Not found' }, status: 404
   end
 
   def default_serializer_options
@@ -23,35 +21,5 @@ class Api::V1::ApplicationController < ActionController::Base
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization'
     headers['Access-Control-Max-Age'] = '1728000'
     headers['Access-Control-Request-Method'] = '*'
-  end
-
-  def authenticate!
-    head :unauthorized unless current_user
-  end
-
-  def authenticate_admin!
-    head :unauthorized unless current_user && current_user.admin?
-  end
-
-  def has_permissions user
-    head :unauthorized unless user == current_user
-  end
-
-  def current_user
-    @current_user
-  end
-
-  def set_current_user
-    token = request.headers['Authorization'].to_s.split(' ').last
-
-    return unless token
-
-    payload = JWT.decode(token, Rails.application.secrets.jwt_secret, 'HS512').first rescue nil
-
-    return unless payload
-
-    if payload && Time.now < Time.at(payload['exp'].to_i)
-      @current_user = User.find(payload['user']['id'])
-    end
   end
 end
